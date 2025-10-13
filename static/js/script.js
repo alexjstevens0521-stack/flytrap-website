6345a02f09d1152da185303760b3f349f5d8c466
Hasan Naqvi <hasansyednaqvi1@gmail.com>
2025-10-13 01:50:22 +0100

    centering

--- static/js/script.js
+++ static/js/script.js
@@ -1,9 +1,9 @@
 const checkBoxOne = document.getElementById("checkboxOne");
 const catImage = document.getElementById("catImage");
 
-checkBoxOne.addEventListener("change", function() {
+checkBoxOne.addEventListener("change", function () {
   if (this.checked) {
-    catImage.style.display = "block";
+    catImage.style.display = "inline";
   } else {
     catImage.style.display = "none";
   }
